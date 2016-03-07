3eb434df50f5f8ee7b8900b4b5bbe50cca81d452
Martin Kearn <martin.kearn@microsoft.com>
2016-03-07 15:52:52 +0000

    Working stream reader

--- ForumTriage/src/ForumTriage-Web/Controllers/HomeController.cs
+++ ForumTriage/src/ForumTriage-Web/Controllers/HomeController.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
 
 namespace ForumTriage_Web.Controllers
 {
@@ -20,16 +23,36 @@ namespace ForumTriage_Web.Controllers
             using (var httpClient = new HttpClient())
             {
                 var apiUrl = (string.Format("http://api.stackexchange.com/2.2/search/advanced?order=desc&sort=activity&accepted=False&tagged={0}&site=stackoverflow", tags[0]));
+                
                 //setup HttpClient
                 httpClient.BaseAddress = new Uri(apiUrl);
                 httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
                 //make request
-                var response = await httpClient.GetAsync(apiUrl);
+                //var response = await httpClient.GetAsync(apiUrl);
+
+                //var response = await httpClient.GetByteArrayAsync(apiUrl);
+                //var str = System.Text.Encoding.UTF8.GetString(response);
+
+                //using (var response = await httpClient.GetStreamAsync(apiUrl))
+                //{
+                //    string contents = response.ReadToEnd();
+                //}
+                var response = string.Empty;
+                using (var stream = await httpClient.GetStreamAsync(apiUrl))
+                {
+                    MemoryStream output = new MemoryStream();
+                    using (GZipStream g = new GZipStream(stream, CompressionMode.Decompress))
+                    {
+                        g.CopyTo(output);
+                    }
+                    response = Encoding.UTF8.GetString(output.ToArray());
+                }
 
                 //read response and write to view
-                var responseContent = await response.Content.ReadAsStreamAsync();
-                ViewData["Result"] = responseContent;
+
+                //var responseContent = await response.Content.ReadAsStreamAsync();
+                //ViewData["Result"] = responseContent;
             }
 
 
